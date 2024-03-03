67909369dfe394c5dbc658858dd8897188551924
kunaldec022002 <kunalingale72@gmail.com>
2024-03-03 20:02:42 +0530

    initial changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './Views/Home/Home';
 
+// 70444052d2d2459c99023e4532137427
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
