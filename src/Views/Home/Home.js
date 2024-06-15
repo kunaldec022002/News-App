14c2766e903d7a4bc0a4c007d40d5b3488e66c64
kunaldec022002 <kunalingale72@gmail.com>
2024-06-15 09:12:18 +0530

    date updating

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -15,7 +15,7 @@ function Home()
 
        try
        {
-        const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-06-10&to=2024-06-10&sortBy=popularity&apiKey=70444052d2d2459c99023e4532137427`);
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-06-14&to=2024-06-14&sortBy=popularity&apiKey=70444052d2d2459c99023e4532137427`);
 
         setNews(response.data.articles);
 
