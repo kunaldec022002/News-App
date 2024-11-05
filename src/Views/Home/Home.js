ca82f444ba1c5e6b43d6ad5e52848bea50441099
kunaldec022002 <kunalingale72@gmail.com>
2024-11-05 22:30:16 +0530

    date update

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -16,7 +16,7 @@ function Home()
 
        try
        {
-        const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-10-20&to=2024-10-20&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API}`)
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-11-03&to=2024-11-04&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API}`)
 
 
 
@@ -37,7 +37,7 @@ function Home()
     useEffect (() =>
     {
         loadNews()
-    },[searchQuery],)
+    },[searchQuery])
 
  
   
